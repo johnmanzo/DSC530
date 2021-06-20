b76e910ca70679861371a6445cf8f10806d8911a
johnmanzo <46502229+johnmanzo@users.noreply.github.com>
2021-06-20 12:02:54 -0500

    ready for grading

---
+++ Week 02/week2JManzo.py
@@ -0,0 +1,53 @@
+# DSC530-301T
+# John Manzo
+# Week 2, Ex. 2.1
+# Jun2 20, 2021
+
+
+# Display the text “Hello World! I wonder why that is always the default coding
+# text to start with”
+greeting = 'Hello World! I wonder why that is always the default coding ' \
+           'text to start with'
+print(greeting)
+
+# Add two numbers together
+num1 = 21
+num2 = 34
+num3 = num1 + num2
+print(f'{num1} + {num2} = {num3}')
+
+# Subtract a number from another number
+num1 = 13
+num2 = 8
+num3 = num1 - num2
+print(f'{num1} - {num2} = {num3}')
+
+# Multiply two numbers
+num1 = 3
+num2 = 5
+num3 = num1 * num2
+print(f'{num1} * {num2} = {num3}')
+
+# Divide between two numbers
+num1 = 55
+num2 = 5
+num3 = num1 / num2
+print(f'{num1} / {num2} = {num3}')
+
+# Concatenate two strings together (any words)
+str1 = 'DSC530'
+str2 = 'Exercise 2.1'
+str3 = str1 + ' ' + str2
+print(str3)
+
+# Create a list of 4 items (can be strings, numbers, both)
+ls = [1, 3, 5, 'Eight']
+print(ls)
+
+# Append an item to your list (again, can be a string, number)
+ls.append(13)
+print(ls)
+
+# Create a tuple with 4 items (can be strings, numbers, both)
+tup = (13, 'Twenty One', 34, 55)
+print(tup)
